2a96e0d8127f8eb8dacd796ed829b8ffc396b78b
Jonathan Dorety <kyullog@gmail.com>
2019-10-16 19:01:33 -0700

    Hopefully fix Queue

--- projects/graph/util.py
+++ projects/graph/util.py
@@ -5,7 +5,7 @@ class Queue():
         self.queue.append(value)
     def dequeue(self):
         if self.size() > 0:
-            return self.queue.pop(0)
+            return self.queue.remove(0)
         else:
             return None
     def size(self):
