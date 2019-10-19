088b5f4455b6d829515b09dfce346cc4fbfbe7e2
Jonathan Dorety <kyullog@gmail.com>
2019-10-19 09:38:28 -0700

    Create family tree graph from input

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -2,4 +2,11 @@ from graph import Graph
 
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    family_tree = Graph()
+    
+    for ancestor in ancestors:
+        if ancestor[0] not in family_tree.vertices.keys():
+            family_tree.add_vertex(ancestor[0])
+        family_tree.add_edge(ancestor[0], ancestor[1])
+    
+    
